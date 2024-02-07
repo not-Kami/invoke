a0bd3ec03f83a4827a61564cf46b21a1a8374ad3
Kami <rokugaki.sama@gmail.com>
2024-02-07 14:50:58 +0100

    first commit

---
+++ src/components/footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2024 Invoke</p>
+    </footer>
+  );
+}
---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>Invoke | Adventure awaits</h1>
+      <nav>
+        {/* Navigation items here */}
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
